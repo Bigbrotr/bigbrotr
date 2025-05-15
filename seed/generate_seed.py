f4e6c26aba8662e71d6b1a30c285d0eeb2b293bb
VincenzoImp <vincenzoimperati99@gmail.com>
2025-05-15 19:05:13 +0200

    update relays_seed generation

--- seed/generate_seed.py
+++ seed/generate_seed.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def generate_seed(relays_url_csv_path, relays_seed_csv_path):
+def generate_seed(relays_url_csv_path):
     relays_url = pd.read_csv(relays_url_csv_path)
     relays_url['domain'] = relays_url['relay_url'].apply(
         lambda x: 'wss://'+x.split("/")[2])
@@ -19,10 +19,11 @@ def generate_seed(relays_url_csv_path, relays_seed_csv_path):
 
     relays_seed = pd.DataFrame.from_dict(relays_seed)
     relays_seed = relays_seed.sort_values(by='count', ascending=False)
-    relays_seed.to_csv(relays_seed_csv_path, index=False)
+    relays_seed.to_csv('relays_seed.csv', index=False)
+    relays_seed['relay_url'].to_csv(
+        'relays_seed.txt', index=False, header=False)
 
 
 if __name__ == '__main__':
     relays_url_csv_path = 'relays_url.csv'
-    relays_seed_csv_path = 'relays_seed.csv'
-    generate_seed(relays_url_csv_path, relays_seed_csv_path)
+    generate_seed(relays_url_csv_path)
